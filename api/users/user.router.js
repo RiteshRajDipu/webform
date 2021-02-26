41c7305b82ec6b5b391d56142917c080ec406f0e
riteshraj <riteshrajr123rs@example.com>
2021-02-26 19:26:54 +0530

    commit again

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,6 +1,18 @@
-const { createUser } = require("./user.controller");
+const { 
+    createUser,
+    getUserByUserId,
+    getUsers,
+    updateUsers,
+    deleteUser,
+    login
+ } = require("./user.controller");
 const router = require("express").Router();
 
-router.post("/", createUser);
 
+router.post("/", createUser);
+router.get("/", getUsers); 
+router.get("/:id", getUserByUserId);
+router.patch("/", updateUsers);
+router.delete("/", deleteUser);  
+router.post("/login", login);
 module.exports = router;
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -3,27 +3,99 @@ const pool = require("../../config/database");
 module.exports = {
     create: (data, callBack) => {
         pool.query(
-         `insert into studs(firstName, lastName, email, number, university, college, year, branch, USN, gender, adress)
+         `insert into students(firstName, lastName, email, password, number, university, college, year, branch, gender, address)
                      values(?,?,?,?,?,?,?,?,?,?,?)`,
           [
               data.first_name,
               data.last_name,
               data.email,
+              data.password, 
               data.number,
               data.university,
               data.college,
               data.year,
-              data.branch, 
-              data.USN,
+              data.branch,
               data.gender,
-              data.adress
+              data.address
           ],
           (error, results, fields) => {
               if (error) {
                  return callBack(error);
               }
-              return callBack(null, results);
+              return callBack(null, results); 
           }            
         );
-    }
+    },
+  getUsers: callBack => {
+      pool.query(
+          `select id,firstName,lastName,email,number,university,college,year,branch,gender,address from students`,
+        [],
+        (error, results, fields) => {
+            if (error) {
+                return callBack(error);
+            }
+            return callBack(null, results);
+        }
+      );
+  },
+   getUserByUserId: (id, callBack) => {
+       pool.query(
+           `select id,firstName,lastName,email,number,university,college,branch,gender,address from students where id = ?`,
+            [id],
+            (error, results, fields) => {
+                if (error) {
+                   return callBack(error);
+                }
+                return callBack(null, results[0]);
+            } 
+           );
+    },
+    updateUser: (data, callBack) => {
+        pool.query(
+         `update students set firstName=?, lastName=?, email=?, password=?, number=?, university=?, college=?, branch=?, gender=?, address=? where id = ?`,
+         [
+             data.first_name,
+             data.last_name,
+             data.email,
+             data.password,
+             data.number,
+             data.university,
+             data.college,
+             data.branch,
+             data.gender,
+             data.address,
+             data.id 
+         ],
+         (error, results, fields) => {
+             if (error) {
+                 callBack(error);
+             }
+             return callBack(null, results); 
+         }   
+        )
+    },
+    deleteUser: (data, callBack) => {
+        pool.query(
+            `delete from students where id =?`,
+            [data.id],
+            (error, results, fields) => {
+                if (error) {
+                   return callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+   getUserByUserEmail: (email, callBack) => {
+       pool.query(
+           `select * from students where email = ?`,
+       [email],
+       (error, results, fields) => {
+           if (error) {
+               callBack(error);
+           }
+           return callBack(null, results[0]);
+       }
+       );
+   }
 };
